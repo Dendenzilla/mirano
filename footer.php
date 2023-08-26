a8c366023966dc78b490e975299268e1633b50c9
Dendenzilla <martinadriaensen@gmail.com>
2023-08-26 18:24:58 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,19 @@
+        </div>
+        </div>
+        <!-- FIN MAIN CONTAINER -->
+        <!-- FOOTER -->
+        <div class="footer">
+
+        </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+            integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
+            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.2/dist/gsap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/ScrollTrigger.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/ScrollToPlugin.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/gh/studio-freight/lenis@0.2.28/bundled/lenis.js"></script>
+        <script src="<?php bloginfo('stylesheet_directory') ?>/js/main.min.js"></script>
+        <?php wp_footer(); ?>
+        </body>
+
+        </html>
---
+++ src/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="keywords" content="Portfolio dekube front-end front end developer web development">
+    <link rel="stylesheet"
+        href="<?php echo get_stylesheet_directory_uri(); ?>/css/style.min.css<?php echo '?' . filemtime( get_stylesheet_directory() . '/css/style.min.css'); ?>"
+        type="text/css" media="screen" />
+    <script src="https://cdn.jsdelivr.net/gh/studio-freight/lenis@0.2.28/bundled/lenis.js"></script>
+    <link rel="icon" href="">
+    <title>Martin Adriaensen's Portfolio</title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <div class="header">
+
+    </div>
+    <!-- FIN HEADER -->
+    <!-- DEBUT MAIN CONT -->
+    <div class="">
+        <div class="content">
