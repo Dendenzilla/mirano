dd3dd6db65c57fb0fd98f21d6f4416975540bdb0
Dendenzilla <martinadriaensen@gmail.com>
2023-10-02 11:25:01 +0200

    pages

---
+++ corporate.php
@@ -0,0 +1,31 @@
+<?php 
+/*
+Template Name: Corporate
+*/
+get_header(); ?>
+
+<h1><?php the_title(); ?></h1>
+
+<div class="corpo__container">
+    <div class="img__holder">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/corpo.jpg" alt="">
+    </div>
+    <div class="txt__holder">
+        <h2>Art blanc</h2>
+        <p>This former cinema offers a breathtaking setting for your corporate events. Its steps, podiums, and its
+            famous rotating dance floor promise to bring back the most beautiful evenings to life! The motorized dance
+            floor takes a central place and allows you to stage a product launch or a show. The balcony under the awning
+            also enables you to showcase your brand and products.</p>
+        <p>Mirano provides flexible spaces including a 95m² stage, a 210m² main room, a 59m² meeting room, and the 64m²
+            Mirano café. Additionally, there are 160m² of extra space with three levels of balconies. The venue features
+            five bars and three private dressing rooms.</p>
+        <p>Mirano offers the opportunity to collaborate with five exclusive caterers, selected for their quality and
+            expertise.</p>
+        <p>Mirano has the capability to organize spectacular artistic and musical shows, create breathtaking floral
+            decorations, and capture these moments with professional photographers. It's also possible to co-produce or
+            script your events. Feel free to share all your wildest desires and specific requests with us.</br>For more
+            information, please refer to the brochure:</p>
+    </div>
+</div>
+
+<?php get_footer(); ?>
